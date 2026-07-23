fcb3a96b2cbe51973e894f905f22927beb6ffc35
gbgnaruto <parmardevraj939@gmail.com>
2026-07-24 04:56:15 +0530

    Configure Express to serve public files

--- sync-server/server.js
+++ sync-server/server.js
@@ -4,6 +4,7 @@ const { Server } = require('socket.io');
 const cors = require('cors');
 
 const app = express();
+app.use(express.static('public'));
 app.use(cors());
 
 const server = http.createServer(app);
